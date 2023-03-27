2ee828c26391c32600aaeb16086222d9a6c971b5
Bishaw Nath Barua <108393747+Bishaw1@users.noreply.github.com>
2023-03-26 21:23:10 -0700

    Hw-7,PostSystem

---
+++ postsys/controller/addPost.php
@@ -0,0 +1,36 @@
+<?php
+//*
+session_start();
+//*
+//*input collection
+$title=$_REQUEST['title'];
+$detail=$_REQUEST['detail'];
+$author=$_REQUEST['author'];
+
+
+$errors=[];
+//*Validation
+
+if (empty($title)) {
+    $errors['title_error']="Apnr title koi?";
+} elseif(strlen($title)>=50){
+    $errors['title_error']="R koto?";
+}
+
+if (empty($detail)) {
+    $errors['detail_error']= "Apnr detail koi?";
+}
+
+if (count($errors)> 0) {
+    //*Redirect back
+    //* header() is redirect or transfer function
+    //*make session
+    $_SESSION['form_errors']=$errors;
+    $_SESSION['old']=$_REQUEST;
+    header("Location:../index.php");
+} else {
+    //*Move forward
+}
+
+
+?>
